7552bfc26b741459b11f1848ba69270db3e2d941
nafizcodes <nimtiaz@ttu.edu>
2022-07-13 19:03:34 -0400

    Added Frontend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
 
-
+//we cannot have anything inside routes tag unless it is a route
 function App() {
   return (
     <>
